346d7e5c16ac01b7a7e8eeb2a4a6b02dbc934e7a
MALIK Alka <alka.malik@soprasteria.com>
2020-01-30 17:53:43 +0530

    added

--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,16 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
-
+const adminData=require('./admin')
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  //console.log('shopjs',adminData.products)
+  const products=adminData.products
+  
+  res.render('shop',{data:products,docTitle:'Shop'})
+  console.log(products[0].title)
+ // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
